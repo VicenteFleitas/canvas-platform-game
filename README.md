8c6952f3ba37a024359cea5402217ac4703dc200
VicenteFleitas <vicente1fleitas1@gmail.com>
2022-02-21 02:33:43 -0300

    okok

---
+++ README.md
@@ -0,0 +1,6 @@
+# canvas platform game with camera made by Vicente Fleitas :)
+- install: npm http-server
+- enter the root forlder and run: http-server
+
+# Render
+- little glich with the camera and collision logic
---
+++ main.js
@@ -0,0 +1,73 @@
+import { rectangleCollision } from "./lib/utils.js";
+import Level from "./lib/Level.js";
+import Player from "./lib/Player.js";
+
+// init canvas
+let canvas = document.getElementById('canvas');
+canvas.setAttribute("width", 512);
+canvas.setAttribute("height", 512);
+canvas.style.background = "#2c3e50";
+let ctx = canvas.getContext('2d');
+
+// vars
+const level = new Level();
+const player = new Player(512/2, 512/2, "#8e44ad");
+
+// init level
+level.createTiles();
+
+// game loop
+function loop() {
+	requestAnimationFrame(loop);
+
+	// update player
+	player.update();
+
+	// update level cam
+	level.x -= player.vx;
+	level.y -= player.vy;
+
+	// collision
+	level.bgTiles.forEach((row, y) => {
+		row.forEach((tile, x) => {
+			if (tile.id === 1 || tile.id === 2) {
+				let newCenterX = (tile.x + level.x) + tile.halfWidth;
+				let newCenterY = (tile.y + level.y) + tile.halfHeight;
+				let updatedTile = { ...tile, centerX: newCenterX, centerY: newCenterY}
+				let collision = rectangleCollision(player, updatedTile);
+				if (collision.hit) {
+					if (collision.side === "left" || collision.side === "right") {
+						level.x += player.vx;
+					}
+					if (player.vy < 0) {
+						if (collision.side === "top") {
+							level.y += player.vy;
+							player.vy = 0;
+						}
+					} 
+					if(player.vy >= 0) {
+						if (collision.side === "bottom") {
+							level.y += player.vy;
+							player.isGround = true;
+							player.vy = 0;
+						}
+					}
+				}
+			}
+		})
+	})
+
+	player.checkGround();
+
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	level.draw(ctx);
+	player.draw(ctx);
+}
+
+loop();
+
+
+
+
+
+
